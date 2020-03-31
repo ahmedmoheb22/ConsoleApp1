a905ec0433b6ec30094e28b72508bed2a6f8c54f
Ahmed moheb <ahmedmoheb500@gmail.com>
2020-03-31 20:16:08 +0200

    THe new change 31 March

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Here is my new change");
         }
     }
 }
